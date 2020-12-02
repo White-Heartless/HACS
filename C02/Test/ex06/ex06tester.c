979dd05296332f90537dfb400335ada7177b0b09
Heartless <fabio.facilla@gmail.com>
2020-12-02 10:27:47 +0100

    ex06tester, improved tests

--- C02/Test/ex06/ex06tester.c
+++ C02/Test/ex06/ex06tester.c
@@ -9,7 +9,7 @@ int main()
     char *p = &str[0];
 
     printf("---Expected-output---\n");
-    printf("0-0-0-0-1-1\n");
+    printf("0-0-0-0-0-1\n");
     printf("----Actual---output---\n");
     strncpy(str, "azAZ\n", 20);
     printf("%d-",ft_str_is_printable(p));
