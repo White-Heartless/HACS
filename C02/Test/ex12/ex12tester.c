17badadc91853c6694929bd4f6f06f70c67fdafc
Heartless <fabio.facilla@gmail.com>
2020-12-02 12:54:14 +0100

    WIP main of ex12

--- C02/Test/ex12/ex12tester.c
+++ C02/Test/ex12/ex12tester.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+void *ft_print_memory(void *addr, unsigned int size);
+
+int ft_strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i];i++)
+		;
+	return(i);
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	char str[] = "According to all1known\1laws of aviation, there2is\2no way a bee should\6be3able to fly.\tIts wings4are\ftoo small to get its fat\nlittle body\roff the9ground.\bThe bee,\aof course,0flies\vanyway,\vbecause bees don't care\rwhat humans think is\aimpossible";
+	ft_print_memory(str, ft_strlen(str));
+
+	return(0);
 }
