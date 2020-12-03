e9b0fa2760ad897f7938fb1a6ff02d01dd804d26
Heartless <fabio.facilla@gmail.com>
2020-12-03 18:16:59 +0100

    fixed capitalization ex11 c02

--- C02/Test/ex11/ex11tester.c
+++ C02/Test/ex11/ex11tester.c
@@ -7,7 +7,7 @@ void ft_putstr_non_printable(char *str);
 int main()
 {
     printf("---Expected-output---\n");
-    printf("What \\0D do \\0A we \\0C need \\09 so \\0B many \\0D characters \\0C for \\09 anyway?\n");
+    printf("What \\0d do \\0a we \\0c need \\09 so \\0b many \\0d characters \\0c for \\09 anyway?\n");
     printf("----Actual---output---\n");
     ft_putstr_non_printable("What \r do \n we \f need \t so \v many \r characters \f for \t anyway?");
     return 0;
