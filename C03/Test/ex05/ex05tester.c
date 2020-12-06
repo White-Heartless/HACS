d1132e7d8dd44a08cce53d96838ab4826459744e
Heartless <fabio.facilla@gmail.com>
2020-12-06 15:57:30 +0100

    this might have fixed a bug. or not

--- C03/Test/ex05/ex05tester.c
+++ C03/Test/ex05/ex05tester.c
@@ -6,21 +6,19 @@ unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
 
 int main()
 {
-    	char string[] = "Heartless is a very\0 error prone person";
-	char string2[] = " handsome guy.";
-	char string0[] = "Heartless is a very\0 error prone person";
-    	char string4[] = "Heartless is\0 a sad boy";
-	char string5[] = "Heartless is\0 a sad boy";
-	char string6[] = " single and ready to mingle.";
+    	char string[150] = "Heartless is a very\0 error prone person";
+	char string2[150] = " handsome guy.";
+	char string0[150] = "Heartless is a very\0 error prone person";
+    	char string4[150] = "Heartless is\0 a sad boy";
+	char string5[150] = "Heartless is\0 a sad boy";
+	char string6[150] = " single and ready to mingle.";
 
 	printf("---Expected-Output---\n");
 	printf("%lu\n",strlcat(string0, string2, 34));
 	printf("%lu\n",strlcat(string4, string6, 39));
-	printf("%s\n", string0);
 	printf("---Actual-Output---\n");
 	printf("%d\n",ft_strlcat(string, string2, 34));
 	printf("%d\n",ft_strlcat(string4, string6, 39));
-	printf("%s\n", string);
 
 	return (0);
 }
