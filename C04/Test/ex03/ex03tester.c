04aab2922238e7beaeb8a165da345d089fb0948b
Heartless <fabio.facilla@gmail.com>
2020-12-07 13:14:41 +0100

    added C04 ex 03 + 05

---
+++ C04/Test/ex03/ex03tester.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_atoi(char *str);
+
+int main(void)
+{
+	printf("---Expected-output---\n");
+	printf("-2147483648__2147483647__0__69__-42\n");
+	printf("----Actual---output---\n");
+	printf("%d__",ft_atoi("  +-+-+-2147483648abc"));
+	printf("%d__",ft_atoi("   ----++--+2147483647asda"));
+	printf("%d__",ft_atoi("++-0"));
+	printf("%d__",ft_atoi("69"));
+	printf("%d\n",ft_atoi("  -+-+-+-+-+--+42aaaaaaaaa"));
+	return 0;
+}
+
---
+++ C04/Test/ex05/ex05tester.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_atoi_base(char *str, char *base);
+
+int main(void)
+{
+	printf("---Expected-output---\n");
+	printf("-2147483648__2147483647__0__69__-42_0_0_0_0\n");
+	printf("----Actual---output---\n");
+	printf("%d__",ft_atoi_base("  +-+-+-cbeiehdgeh123","abcdefgihj"));
+	printf("%d__",ft_atoi_base("   ----++--+7FFFFFFFa","0123456789ABCDEF"));
+	printf("%d__",ft_atoi_base("++-n","nul"));
+	printf("%d__",ft_atoi_base("baf","abcdefgh"));
+	printf("%d_",ft_atoi_base("  -+-+-+-+-+--+ahahah","ha"));
+	printf("%d_",ft_atoi_base(" +-+-++---+--+xd","asdfghj+qwert"));
+	printf("%d_",ft_atoi_base(" -+asdfg","asdfg-hjkl"));
+	printf("%d_",ft_atoi_base(" -+-+++-","asdfg hjkl"));
+	printf("%d\n",ft_atoi_base(" -+-+++-","asdfghjkdl"));
+	return 0;
+}
