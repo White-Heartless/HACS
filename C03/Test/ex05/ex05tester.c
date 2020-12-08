4c54628c2be8a0a4629e509a169c386c8a15defa
Heartless <fabio.facilla@gmail.com>
2020-12-08 10:09:23 +0100

    fixed a typo

--- C03/Test/ex05/ex05tester.c
+++ C03/Test/ex05/ex05tester.c
@@ -10,7 +10,7 @@ int main()
 	char string2[150] = " handsome guy.";
 	char string0[150] = "Heartless is a very\0 error prone person";
     	char string4[150] = "Heartless is\0 a sad boy";
-	//char string5[150] = "Heartless is\0 a sad boy";
+	char string5[150] = "Heartless is\0 a sad boy";
 	char string6[150] = " single and ready to mingle.";
 
 	printf("---Expected-Output---\n");
@@ -18,7 +18,7 @@ int main()
 	printf("%lu\n",strlcat(string4, string6, 39));
 	printf("---Actual-Output---\n");
 	printf("%d\n",ft_strlcat(string, string2, 34));
-	printf("%d\n",ft_strlcat(string4, string6, 39));
+	printf("%d\n",ft_strlcat(string5, string6, 39));
 
 	return (0);
 }
